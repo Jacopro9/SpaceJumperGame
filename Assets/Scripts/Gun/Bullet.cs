a0a17f708ab77c750fbb20d7b4e41a7f5871ad67
Jacopro9 <jajwillcocks@live.com>
2022-12-13 16:40:36 +0000

    bullet delete and impact explosion

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactPrefab;
     public float speed = 10f;
 
     public int damage = 35;
@@ -13,6 +14,7 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rb.velocity = transform.right * speed;
+        Object.Destroy(gameObject, 2.0f);
     }
 
     void OnTriggerEnter2D (Collider2D hitInfo)
@@ -25,6 +27,7 @@ public class Bullet : MonoBehaviour
         if (!hitInfo.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            Instantiate(bulletImpactPrefab, transform.position, transform.rotation);
         }
     }
 }
---
+++ Assets/Scripts/Gun/BulletImpact.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletImpact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.Destroy(gameObject, 0.1f);
+    }
+
+}
