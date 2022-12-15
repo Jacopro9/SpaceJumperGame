2fed9736b02fc77e08182730cfa6a8d3f0e8c2d2
Jacopro9 <jajwillcocks@live.com>
2022-12-15 11:38:50 +0000

    shooting sounds

--- Assets/Scripts/Gun/BulletImpact.cs
+++ Assets/Scripts/Gun/BulletImpact.cs
@@ -7,6 +7,7 @@ public class BulletImpact : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        FindObjectOfType<AudioManager>().Play("BulletImpact");
         Object.Destroy(gameObject, 0.3f);
     }
 
